f4d2ea77a280ca218f6c134189fcfe4812348089
bxnvo <brandonvo27771@gmail.com>
2023-10-19 22:22:26 -0700

    completed up to lab exercise

--- examples/simple.cpp
+++ examples/simple.cpp
@@ -1,6 +1,8 @@
 int main() {
     int *p = new int[10];
-    p[10] = 1;
+    p[9] = 1; // Correct this to size -1 = 10 - 1 = 9 instead of 10
 
+    //delete p;
+    delete[] p; // Deallocate memory from new allocation above
     return 0;
 }
--- examples/uninitialized.cpp
+++ examples/uninitialized.cpp
@@ -2,8 +2,46 @@
 using namespace std;
 
 int main() {
-    int x;
+    int x = 10;
     cout << x << endl;
 
     return 0;
+} 
+
+/* #include <iostream>
+using namespace std;
+
+ int main() {
+    int x;
+    bool z;
+    int y = x + 5;
+    if (x) {
+        cout << "X is non-zero" << endl;
+    }
+    if (z) {
+        cout << "Z is truthy" << endl;
+    }
+    cout << y << endl;
+    return 0;
+} 
+
+#include <iostream>
+
+void foo(int x)
+{
+    std::cout<<x<<std::endl;
 }
+
+int main()
+{
+    int x;
+    int r=4;
+    int y=x+r;
+    if(x) x++; // uninitialized error reported here
+    if(r) r++;
+    if(y) y++; // uninitialized error reported here
+
+    foo(y);
+    
+    return 0;
+} */
